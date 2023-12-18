90c1e96977f827f75a3e213fa168d289dd595249
Chofniknol <48439956+Chofniknol@users.noreply.github.com>
2023-12-18 19:44:27 +0100

    testing connection with digitalocean database

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,3 +3,4 @@ export const environment = {
   BASIC_URL : "https://iprwc-backend-api.onrender.com/"
 };
 // need this for testing: BASIC_URL : "http://localhost:8080/"
+// BASIC_URL : "https://iprwc-backend-api.onrender.com/"
