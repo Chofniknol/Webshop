fa52eb8c6ecf253917461881e2c854056d268221
Chofniknol <48439956+Chofniknol@users.noreply.github.com>
2023-12-18 17:02:41 +0100

    Connected the front end with online backend service on render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  BASIC_URL : "http://localhost:8080/" //added this here because error Property 'BASIC_URL' does not exist on type '{ production: boolean; }'
+  BASIC_URL : "https://iprwc-backend-api.onrender.com/" //added this here because error Property 'BASIC_URL' does not exist on type '{ production: boolean; }'
 };
+
+// need this for testing: BASIC_URL : "http://localhost:8080/"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  BASIC_URL : "http://localhost:8080/"
+  BASIC_URL : "https://iprwc-backend-api.onrender.com/"
 };
+// need this for testing: BASIC_URL : "http://localhost:8080/"
